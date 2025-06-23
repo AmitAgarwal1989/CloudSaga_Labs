dad6165e669998726dc594f8b83dee0bdf738f91
Amit Agarwal <1989amit@gmail.com>
2025-06-23 18:52:03 +0530

    Adding the Subnet, IGW and Route Table

--- main.tf
+++ main.tf
@@ -1,3 +1,29 @@
 resource "aws_vpc" "myvpc" {
-  cidr_block = "10.0.0.0/24"
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "mysubnet1" {
+  vpc_id = aws_vpc.myvpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "ap-south-1a"
+}
+
+resource "aws_subnet" "mysubnet2" {
+  vpc_id = aws_vpc.myvpc.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "ap-south-1b"
+}
+
+resource "aws_internet_gateway" "myigw" {
+  vpc_id = aws_vpc.myvpc.id
+}
+
+resource "aws_route_table" "myroute1" {
+  vpc_id = aws_vpc.myvpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myigw.id
+  }
+  depends_on = [aws_internet_gateway.myigw]
 }
