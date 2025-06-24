0456d1d6024ab419f84fa848dfa981af919ea75e
Amit Agarwal <1989amit@gmail.com>
2025-06-24 13:01:05 +0530

    Updated the NatGateway and RT Association

--- main.tf
+++ main.tf
@@ -20,10 +20,31 @@ resource "aws_internet_gateway" "myigw" {
 
 resource "aws_route_table" "myroute1" {
   vpc_id = aws_vpc.myvpc.id
-
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.myigw.id
   }
   depends_on = [aws_internet_gateway.myigw]
+}
+resource "aws_route_table_association" "publicsubnet" {
+  subnet_id = aws_subnet.mysubnet1.id
+  route_table_id = aws_route_table.myroute1.id
+}
+resource "aws_route_table_association" "privatesubnet" {
+  subnet_id = aws_subnet.mysubnet2.id
+  route_table_id = aws_vpc.myvpc.main_route_table_id
+}
+resource "aws_eip" "myeip" {
+  domain = "vpc"
+}
+resource "aws_nat_gateway" "myngw" {
+  depends_on = [ aws_eip.myeip ]
+  allocation_id = aws_eip.myeip.id
+  subnet_id = aws_subnet.mysubnet2.id
+}
+resource "aws_route" "natgateway" {
+  route_table_id = aws_vpc.myvpc.main_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_nat_gateway.myngw.id
+  
 }
